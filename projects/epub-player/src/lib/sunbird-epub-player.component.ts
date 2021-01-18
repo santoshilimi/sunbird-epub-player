6317d1a7a8fefeb368ce7f5eec0bd90906233cf0
diptesh6501 <diptesh1993@gmail.com>
2021-01-18 16:13:39 +0530

    Issue #SB-22286 fix: initial commit for epub player

---
+++ projects/epub-player/src/lib/sunbird-epub-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sunbird-epub-player',
+  templateUrl: './sunbird-epub-player.component.html',
+  styles: []
+})
+export class EpubPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/epub-player/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of epub-player
+ */
+
+export * from './lib/sunbird-epub-player.service';
+export * from './lib/sunbird-epub-player.component';
+export * from './lib/sunbird-epub-player.module';
