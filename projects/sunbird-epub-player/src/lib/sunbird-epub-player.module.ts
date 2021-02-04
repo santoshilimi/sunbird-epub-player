95346c4ff5e09297bf69042454b19f4548fff617
diptesh6501 <diptesh1993@gmail.com>
2021-02-04 11:57:24 +0530

    Issue #SB-22286 feat: changed library name

--- projects/sunbird-epub-player/src/lib/sunbird-epub-player.module.ts
+++ projects/sunbird-epub-player/src/lib/sunbird-epub-player.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { EpubPlayerComponent } from './sunbird-epub-player.component';
 import { EpubViewerComponent } from './epub-viewer/epub-viewer.component';
-import { SunbirdPlayerSdkModule  } from 'player-sdk';
+import { SunbirdPlayerSdkModule  } from '@project-sunbird/sunbird-player-sdk';
 import { CommonModule } from '@angular/common';
 
 
